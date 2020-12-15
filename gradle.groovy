d5973d6053ebb5b818aaac9abfce9644120eaa4c
DonCarlosRiveros <carlos.riveros@gmail.com>
2020-12-15 17:26:31 -0300

    Add files via upload

---
+++ gradle.groovy
@@ -0,0 +1,25 @@
+def call()
+{
+	stage('Build')
+	{
+		sh 'gradle clean build'
+	}
+	stage('Sonar')
+	{
+		def sonarhome = tool 'sonar'
+        sh "/opt/sonar-scanner/bin/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+	}
+	stage('Run')
+	{
+		sh "nohup gradle bootRun &"
+	}
+	stage('Rest')
+	{
+	    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+	}
+	stage('Nexus')
+	{
+		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	}
+}
+return this;
