62820c75fa60d6169af8a7e765e7b743d84f0ff2
DonCarlosRiveros <carlos.riveros@gmail.com>
2020-12-16 11:22:53 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -14,9 +14,9 @@ def llamada()
     }
     stage('Sonar')
     {
-        def sonarhome = tool('sonarscanner')
+        def sonarhome = tool('SonarQubeScanner')
         {
-            withSonarQubeEnv('sonarscanner')
+            withSonarQubeEnv('sonar')
             {
                 sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
             }
