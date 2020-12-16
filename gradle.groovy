82ed168dc6599bdd45749f1c84545306e2ccaff9
Carlos M. Riveros C <carlos.riveros@gmail.com>
2020-12-16 09:49:33 -0300

    se agregan archivos groovy

---
+++ gradle.groovy
@@ -0,0 +1,30 @@
+def llamada_gradle()
+{
+	stage('Build')
+	{
+		sh 'gradle clean build'
+	}
+	stage('Sonar')
+	{
+		def sonarhome = tool 'sonar'
+		{
+			withSonarQubeEnv('sonar')
+			{
+				sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+			}
+		}
+	}
+	stage('Run')
+	{
+		sh "nohup gradle bootRun &"
+	}
+	stage('Rest')
+	{
+	    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+	}
+	stage('Nexus')
+	{
+		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	}
+}
+return this;
---
+++ maven.groovy
@@ -0,0 +1,30 @@
+def llamada_maven()
+{
+    stage('Compile')
+    {
+        sh './mvnw clean compile -e'   
+    }
+    stage('Test')
+    {
+        sh './mvnw clean test -e'   
+    }
+    stage('Jar')
+    {
+        sh './mvnw clean package -e'
+    }
+    stage('Sonar')
+    {
+        def sonarhome = tool 'sonar'
+        {
+            withSonarQubeEnv('sonar')
+            {
+                sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+            }
+        }
+    }
+    stage('Nexus')
+    {
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]] 
+    }
+}
+return this;
