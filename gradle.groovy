f57faaf6553dca77de13ad412101a7eca5961f97
DonCarlosRiveros <carlos.riveros@gmail.com>
2020-12-17 09:43:26 -0300

    Update gradle.groovy
    Se incorpora uso variable global para el manejo del nombre del stage en caso de error en éste

--- gradle.groovy
+++ gradle.groovy
@@ -1,28 +1,34 @@
-def llamada()
+def llamada_gradle()
 {
 	stage('Build')
 	{
+		NOMBRE_STAGE = 'Build'
 		sh 'gradle clean build'
 	}
 	stage('Sonar')
 	{
 		def sonarhome = tool 'sonar'
+		{
 			withSonarQubeEnv('sonar')
 			{
 				sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+			}
 		}
+		NOMBRE_STAGE = 'Sonar'
 	}
 	stage('Run')
 	{
+		NOMBRE_STAGE = 'Run'
 		sh "nohup gradle bootRun &"
 	}
 	stage('Rest')
 	{
-		sh "sleep 10"
-		sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+	    NOMBRE_STAGE = 'Rest'
+	    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
 	}
 	stage('Nexus')
 	{
+		NOMBRE_STAGE = 'Nexus'
 		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 	}
 }
