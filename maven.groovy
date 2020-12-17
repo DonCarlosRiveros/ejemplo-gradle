6e2270064830e764c6202a5e1d8adfcdc5fca97c
DonCarlosRiveros <carlos.riveros@gmail.com>
2020-12-17 11:10:36 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -2,17 +2,17 @@ def llamada_maven()
 {
     stage('Compile')
     {
-        NOMBRE_STAGE = 'Compile'
+        ${NOMBRE_STAGE} = 'Compile'
         sh './mvnw clean compile -e'   
     }
     stage('Test')
     {
-        NOMBRE_STAGE = 'Test'
+        ${NOMBRE_STAGE} = 'Test'
         sh './mvnw clean test -e'   
     }
     stage('Jar')
     {
-        NOMBRE_STAGE = 'Jar'
+        ${NOMBRE_STAGE} = 'Jar'
         sh './mvnw clean package -e'
     }
     stage('Sonar')
@@ -21,15 +21,15 @@ def llamada_maven()
         {
             withSonarQubeEnv('sonar')
             {
-                NOMBRE_STAGE = 'Sonar'
+                ${NOMBRE_STAGE} = 'Sonar'
                 sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
             }
         }
     }
     stage('Nexus')
     {
-        NOMBRE_STAGE = 'Nexus'
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.0']]] 
+        ${NOMBRE_STAGE} = 'Nexus'
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]] 
     }
 }
 return this;
