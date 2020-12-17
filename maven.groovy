85af68f99bfe26e8a0c0305fc1a4fc82658f9c85
DonCarlosRiveros <carlos.riveros@gmail.com>
2020-12-17 09:44:32 -0300

    Update maven.groovy
    Se incorpora uso variable global para el manejo del nombre del stage en caso de error en éste

--- maven.groovy
+++ maven.groovy
@@ -1,28 +1,34 @@
-def llamada()
+def llamada_maven()
 {
     stage('Compile')
     {
-        sh 'mvn clean compile -e'   
+        NOMBRE_STAGE = 'Compile'
+        sh './mvnw clean compile -e'   
     }
     stage('Test')
     {
-        sh 'mvn clean test -e'   
+        NOMBRE_STAGE = 'Test'
+        sh './mvnw clean test -e'   
     }
     stage('Jar')
     {
-        sh 'mvn clean package -e'
+        NOMBRE_STAGE = 'Jar'
+        sh './mvnw clean package -e'
     }
     stage('Sonar')
     {
-        def sonarhome = tool('sonar')
+        def sonarhome = tool 'sonar'
+        {
             withSonarQubeEnv('sonar')
             {
-                /* sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build" */
-                sh '/opt/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+                NOMBRE_STAGE = 'Sonar'
+                sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
             }
+        }
     }
     stage('Nexus')
     {
+        NOMBRE_STAGE = 'Nexus'
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]] 
     }
 }
